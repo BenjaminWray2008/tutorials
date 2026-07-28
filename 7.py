1ef3438f3f671c77978ef7f85b67e6d3024fb2e7
Ben Gorman <bengorman@Bens-MacBook-Neo.local>
2026-07-29 08:13:00 +1200

    added fibo

--- 7.py
+++ 7.py
@@ -1,6 +1,10 @@
-def example_7(n: int):
-    if n <= 1:
+def fibonacci(n: int):
+    
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-
-    # Each call creates TWO new calls
-    return example_7(n - 1) + example_7(n - 1)
+    else:
+        #  Each call creates TWO!! new calls
+        fibonacci_N = fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci_N
